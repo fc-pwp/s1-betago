93debbeb61e84d5c2cd76ddac4a00607ccf27cb2
Kay Cha <kay@hannal.net>
2016-03-30 22:40:59 +0900

    add 문항에 답하고 세션에 담기

--- quiz/views.py
+++ quiz/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 
+from .models import Answer
 from .models import Quiz
+from .models import UserResult
 from .forms import UserResultForm
 from .forms import AnswerForm
 
@@ -26,6 +28,8 @@ def quiz_start(request, pk):
         if form.is_valid():
             new_user_result = form.save(commit=False)
             new_user_result.quiz = quiz
+            new_user_result.save()
+            request.session['user_result_pk'] = new_user_result.pk
             return redirect('question_view', pk=quiz.pk, order=1)
 
     ctx = {
@@ -44,10 +48,46 @@ def question_view(request, pk, order):
 
     question = qs[0]  # or `qs.first()`
 
+    session_key_name = 'quiz{}_question'.format(quiz.pk)
+
+    if session_key_name not in request.session:
+        if question.order > 1:
+            return redirect('quiz_start', pk=quiz.pk)
+        else:
+            request.session[session_key_name] = question.order - 1
+    else:
+        if request.session[session_key_name] + 1 > question.order:
+            return redirect(
+                'question_view',
+                pk=quiz.pk,
+                order=question.order+1
+            )
+
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
-            pass
+            if request.session[session_key_name] == question.order:
+                return redirect(
+                    'question_view',
+                    pk=quiz.pk,
+                    order=question.order+1
+                )
+            user_result_pk = request.session['user_result_pk']
+            user_result = get_object_or_404(UserResult, pk=user_result_pk)
+            answer = get_object_or_404(
+                Answer,
+                question=question,
+                order=form.cleaned_data['order']
+            )
+            user_result.scores += answer.score
+            user_result.save()
+            request.session[session_key_name] = question.order
+            return redirect(
+                'question_view',
+                pk=quiz.pk,
+                order=question.order+1
+            )
+
     elif request.method == 'GET':
         form = AnswerForm()
 
